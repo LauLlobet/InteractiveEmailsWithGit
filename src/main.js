cf8afd67e44cced4aa6808d455511afcb733db8e
Oscar V <oscarvalcarcelmorales@hotmail.com>
2017-07-18 20:07:39 +0200

    test

--- src/main.js
+++ src/main.js
@@ -8,4 +8,9 @@ app.post('/', function(request, response){
   response.send(request.body);    // echo the result back
 });
 
+app.get('/', function(req, res) {
+
+  res.send("TEST");
+});
+
 app.listen(3000);
