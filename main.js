fd9e61ffdb1fe5b26a80e86b495d851414c11b19
Oscar V <oscarvalcarcelmorales@hotmail.com>
2017-07-18 20:11:05 +0200

    test

--- main.js
+++ main.js
@@ -6,16 +6,12 @@ var http = require("http").Server(app);
 var request = require('request');
 
 
-app.post('/',function(){
-		console.log("sending email");
-		var url  = "https://api.sendgrid.com/v3/mail/send";
-		var headerVal = "Bearer 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" ;
-		var data  = '{"personalizations": [{"to": [{"email": "lau.llobet@gmail.com"}]}],"from": {"email": "lau.llobet@gmail.com"},"subject": "Hello, World!","content": [{"type": "text/plain", "value": "Heya!Yupiiiiiii"}]}';
+app.post('/', function(request, response){
+  console.log(request.body);      // your JSON
+  response.send(request.body);    // echo the result back
+});
 
-		postJSON(data,url,headerVal);
 
-	});
-
-http.listen(8888,function(){
+http.listen(8080,function(){
 	console.log("escoltant");	
 })
